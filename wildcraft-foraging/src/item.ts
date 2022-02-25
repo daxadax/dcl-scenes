a666482ffd1d70a502a9c00040a13df2ab9d6cf7
dax <d.dax@email.com>
2022-02-25 12:30:08 +0100

    Remove 'foraging'

--- wildcraft-foraging/src/item.ts
+++
@@ -1,9 +0,0 @@
-// import * as ui from '@dcl/ui-scene-utils'
-
-export class Item {
-  name!: String;
-  description!: String;
-  quantity!: Number;
-  display_name!: String;
-  imgPath!: String;
-}
